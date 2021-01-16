2cb0bb4cfe27643b421f4f07786b5c1e055618ae
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-16 15:56:13 -0700

    Add traveler constructor test

--- test-data/traveler-test-data.js
+++ test-data/traveler-test-data.js
@@ -1,4 +1,4 @@
-export const testUsers = [
+export const testTravelers = [
   {
     "id": 1,
     "name": "Ham Leadbeater",
--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,10 +1,22 @@
 import chai from 'chai';
-import { testUsers } from '../test-data/traveler-test-data.js';
+import { testtravelers } from '../test-data/traveler-test-data.js';
 
 const expect = chai.expect;
 
 describe('a Traveler', () => {
-  it('should be able to import testUsers', () => {
-    expect(testUsers).to.be.an('Array');
+  let traveler;
+
+  beforeEach(() => {
+    traveler = new Traveler(testTravelers[testTravelers.length - 1]);    
+  });
+
+  it('should be able to import testtravelers', () => {
+    expect(testtravelers).to.be.an('Array');
+  });
+
+  it('should have an appropriate constructor', () => {
+    expect(traveler.name).to.eql('Morey Flanders');
+    expect(traveler.id).to.eql(50);
+    expect(traveler.type).to.eql('foodie');
   });
 });
