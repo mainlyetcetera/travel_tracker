8998f3ba2c3a03d0e9f30b428ae0a900bfc314d0
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-16 15:28:06 -0700

    Add users test data

---
+++ user-test-data.js
@@ -0,0 +1,42 @@
+export const testUsers = [
+  {
+    "id": 1,
+    "name": "Ham Leadbeater",
+    "travelerType": "relaxer"
+  },
+  {
+    "id": 2,
+    "name": "Rachael Vaughten",
+    "travelerType": "thrill-seeker"
+  },
+  {
+    "id": 3,
+    "name": "Sibby Dawidowitsch",
+    "travelerType": "shopper"
+  },
+  {
+    "id": 17,
+    "name": "Julian Ullyott",
+    "travelerType": "relaxer"
+  },
+  {
+    "id": 47,
+    "name": "Den Dossettor",
+    "travelerType": "relaxer"
+  },
+  {
+    "id": 48,
+    "name": "Lurlene Raisbeck",
+    "travelerType": "photographer"
+  },
+  {
+    "id": 49,
+    "name": "Guthry Tute",
+    "travelerType": "thrill-seeker"
+  },
+  {
+    "id": 50,
+    "name": "Morey Flanders",
+    "travelerType": "foodie"
+  }
+]
