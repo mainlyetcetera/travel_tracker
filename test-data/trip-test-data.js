2dfde43b517fbf421cbe91f4697692a1b61e2a05
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-16 15:33:36 -0700

    Add trip test data

---
+++ test-data/trip-test-data.js
@@ -0,0 +1,32 @@
+export const testTrips = [
+  {
+    "id": 93,
+    "userID": 40,
+    "destinationID": 47,
+    "travelers": 2,
+    "date": "2019/12/08",
+    "duration": 16,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 152,
+    "userID": 30,
+    "destinationID": 40,
+    "travelers": 1,
+    "date": "2020/03/26",
+    "duration": 16,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 199,
+    "userID": 45,
+    "destinationID": 39,
+    "travelers": 6,
+    "date": "2020/05/17",
+    "duration": 6,
+    "status": "pending",
+    "suggestedActivities": []
+  }
+];
