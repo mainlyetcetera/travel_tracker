e55cf7ef5765a979bca30821c679d2310f7acd74
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-16 16:30:21 -0700

    Add test for trip containers and instance

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
-import { testtravelers } from '../test-data/traveler-test-data.js';
+import { testTravelers } from '../test-data/traveler-test-data.js';
+import Traveler from '../src/data-handling/Traveler.js';
 
 const expect = chai.expect;
 
@@ -10,13 +11,24 @@ describe('a Traveler', () => {
     traveler = new Traveler(testTravelers[testTravelers.length - 1]);    
   });
 
-  it('should be able to import testtravelers', () => {
-    expect(testtravelers).to.be.an('Array');
+  it('should be an instance of Traveler', () => {
+    expect(traveler).to.be.an.instanceof(Traveler);
+  });
+
+  it('should be able to import testTravelers', () => {
+    expect(testTravelers).to.be.an('Array');
   });
 
   it('should have an appropriate constructor', () => {
     expect(traveler.name).to.eql('Morey Flanders');
     expect(traveler.id).to.eql(50);
     expect(traveler.type).to.eql('foodie');
+  });
+
+  it('should have a place to store various types of trips', () => {
+    expect(traveler.pastTrips).to.be.an('Array');
+    expect(traveler.currentTrips).to.be.an('Array');
+    expect(traveler.upcomingTrips).to.be.an('Array');
+    expect(traveler.pendingTrips).to.be.an('Array');
   });
 });
