f1421b8396f0396751ec33bfb46c9dbb48ac3640
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-18 14:03:00 -0700

    Parse initial data in index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import Traveler from './data-handling/Traveler.js';
+import Trip from './data-handling/Trip.js';
+
 import { getData } from '../utils/api/apiCalls.js';
 
-// console.log(getData('travelers'));
+let traveler;
+let trips;
+let destinations;
 
 const initiateData = () => {
   const travelersPromise = getData('travelers');
   const tripsPromise = getData('trips');
   const destinationsPromise = getData('destinations');
+  const promises = [travelersPromise, tripsPromise, destinationsPromise];
+  Promise.all(promises)
+    .then(data => {      
+      const index = getRandomIndex(data[0].travelers);      
+      traveler = new Traveler(data[0].travelers[index]);
+      trips = data[1].trips.map(trip => new Trip(trip));
+      destinations = data[2].destinations;      
+    });
 }
 
-initiateData();
+const getRandomIndex = list => Math.floor(Math.random() * list.length);
 
-console.log('This is the JavaScript entry file - your code begins here.');
+window.onload = initiateData;
