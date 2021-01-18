01a6a22f196d643b840818b5199789bfa68e3c63
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-18 13:41:55 -0700

    Setup first promises for datasets

--- src/index.js
+++ src/index.js
@@ -7,4 +7,16 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import { getData } from '../utils/api/apiCalls.js';
+
+// console.log(getData('travelers'));
+
+const initiateData = () => {
+  const travelersPromise = getData('travelers');
+  const tripsPromise = getData('trips');
+  const destinationsPromise = getData('destinations');
+}
+
+initiateData();
+
 console.log('This is the JavaScript entry file - your code begins here.');
