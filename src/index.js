38eb1993ee13dd4afe4f1aba44b7704890a55092
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-19 08:48:58 -0700

    Access trips after making trip with post request

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './images/turing-logo.png'
 import Traveler from './data-handling/Traveler.js';
 import Trip from './data-handling/Trip.js';
 
-import { getData } from '../utils/api/apiCalls.js';
+import { getData, postData } from '../utils/api/apiCalls.js';
 import { domUpdates } from './domUpdates.js';
 
 const dateInput = document.querySelector('.date-input');
 const durationInput = document.querySelector('.duration-input');
 const travelersInput = document.querySelector('.travelers-input');
+const destinationInput = document.querySelector('.destinations-drop-down');
+const makeTripButton = document.querySelector('.make-trip');
 
 let traveler;
 let travelers;
@@ -52,4 +54,49 @@ const generateTravelerGrandTotal = (traveler, destinations) => {
   totalDisplay.innerText = `You have spent $${total} on trips this year!`;
 }
 
+const makeTrip = event => {
+  event.preventDefault();
+  // const startDate = dateInput.value;
+  // const duration = durationInput.value;
+  // const travelers = travelersInput.value;
+  // const destination = destinationInput.value;
+
+  // find greatest id among trips, get next one higher
+  // get traveler's id
+  // get id of destination whose destination prop matches value
+
+  const values = {
+    id: Date.now(),
+    userID: 23,
+    destinationID: 24,
+    travelers: 2,
+    date: '2021/04/12',
+    duration: 4,
+    status: 'pending',
+    suggestedActivities: []
+  };
+
+  const sent = postData(values);
+  const received = getData('trips');
+  const promises = [sent, received];
+  console.log(promises);
+  Promise.all(promises)    
+    .then(data => console.log(data));
+}
+
+/*
+pattern to send
+{
+  id: <number>, 
+  userID: <number>, 
+  destinationID: <number>, 
+  travelers: <number>, 
+  date: <string 'YYYY/MM/DD'>, 
+  duration: <number>, 
+  status: <string 'approved' or 'pending'>, 
+  suggestedActivities: <array of strings>
+}
+*/
+
 window.onload = initiateData;
+makeTripButton.addEventListener('click', makeTrip);
