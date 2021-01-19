3118ab5a4254e377b3d26ff4f0df1f2eebce77af
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-18 18:41:28 -0700

    Display traveler's name

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,1 +1,8 @@
-export const domUpdates = {};
+import Traveler from './data-handling/Traveler.js';
+
+export const domUpdates = {
+  displayUserMsg(traveler) {
+    const userMsg = document.querySelector('.welcome-msg');    
+    userMsg.innerText = `Welcome ${traveler.returnFirstName()}!`;
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 import Traveler from './data-handling/Traveler.js';
 import Trip from './data-handling/Trip.js';
 
 import { getData } from '../utils/api/apiCalls.js';
+import { domUpdates } from './domUpdates.js';
 
 let traveler;
 let trips;
@@ -23,11 +21,14 @@ const initiateData = () => {
       const index = getRandomIndex(data[0].travelers);      
       traveler = new Traveler(data[0].travelers[index]);
       trips = data[1].trips.map(trip => new Trip(trip));
-      destinations = data[2].destinations;      
+      destinations = data[2].destinations;
+      domUpdates.displayUserMsg(traveler);      
     })
     .catch(err => console.log(err));
 }
 
 const getRandomIndex = list => Math.floor(Math.random() * list.length);
+
+
 
 window.onload = initiateData;
